eda3fde66f0fe8c491ebc6b2c7e7eadc4de0d1b4
Thomas <thomas.baerentsen@gmail.com>
2020-02-14 09:24:34 +0100

    Implemented interfaces

---
+++ Refactored_ECS/Refactored_ECS/Heater/HeaterFake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactored_ECS.Heater
+{
+    class HeaterFake : IHeater
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("HeaterFake turned on");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Heater fake turned off");
+        }
+
+        public bool RunSelfTest()
+        {
+            Console.WriteLine("Heater fake running selftest");
+            return true;
+        }
+    }
+}
---
+++ Refactored_ECS/Refactored_ECS/TempSensor/TempSensorFake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactored_ECS.TempSensor
+{
+    class TempSensorFake : ITempSensor
+    {
+        public int GetTemp()
+        {
+            //Always returns 22 for testing purposes
+            return 22;
+        }
+
+        public bool RunSelfTest()
+        {
+            Console.WriteLine("TempSensorFake running selftest");
+            return true;
+        }
+    }
+}
