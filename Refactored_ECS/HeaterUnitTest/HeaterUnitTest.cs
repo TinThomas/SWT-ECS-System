90d9e9b874d848546216768bf1848d1766fcddf1
Thomas <thomas.baerentsen@gmail.com>
2020-02-14 09:35:13 +0100

    Refactored

--- Refactored_ECS/HeaterUnitTest/HeaterUnitTest.cs
+++ Refactored_ECS/HeaterUnitTest/HeaterUnitTest.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Refactored_ECS.Heater;
 
 namespace HeaterUnitTest
 {
+    [TestFixture]
     public class HeaterUnitTest
     {
+        private IHeater uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            uut = new HeaterFake();
+        }
+
+        [Test]
+        public void test()
+        {
+
+        }
     }
 }
